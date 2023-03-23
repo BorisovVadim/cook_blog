e92c2c5c2374b04be6f9fb2fb0679d599fca38b3
vadimborisov <vadimborisovishere@gmail.com>
2023-03-23 10:00:32 +0300

    Added home.html, added HomeView to views.py and to path at urls.py q

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,12 @@ from django.views.generic import ListView, DetailView
 from .models import Post
 
 
+class HomeView(ListView):
+    model = Post
+    paginate_by = 6
+    template_name = 'blog/home.html'
+
+
 class PostListView(ListView):
     model = Post
 
@@ -15,9 +21,5 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
-    context_object_name = 'post'  # определяем название объекта в шаблоне
+    context_object_name = 'post'  # Определяем название объекта в шаблоне
     slug_url_kwarg = 'post_slug'
-
-
-def home(request):
-    return render(request, 'base.html')
