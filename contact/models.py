f459b709cf869ea4b8ef11765ed587e20fc0edcd
vadimborisov <vadimborisovishere@gmail.com>
2023-03-29 13:00:10 +0300

    Добавил возможность вводить свои контакты и сохранять их в БД

--- contact/models.py
+++ contact/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class ContactModel(models.Model):
     """Класс модели обратной связи"""
     name = models.CharField(max_length=50)
-    email = models.EmailField()
-    website = models.URLField()
+    email = models.EmailField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
     message = models.TextField(max_length=5000)
     create_at = models.DateTimeField(auto_now_add=True)
 
