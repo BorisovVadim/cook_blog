ec9b430ac2cdc99f923286b9d5a53715c23581e7
vadimborisov <vadimborisovishere@gmail.com>
2023-04-05 10:12:08 +0300

    Добавил виждиты для отображения плейсхолдеров в форма комментарием на странице с постами

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ['create_at', 'post']  # исключаем поля create_at и post для отображения в форме
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'website': forms.TextInput(attrs={'placeholder': 'Website'}),
+            'message': forms.Textarea(attrs={'placeholder': 'Message'})
+        }  # виджет для отображения placeholder'ов в полях в шаблоне post_detail.html
